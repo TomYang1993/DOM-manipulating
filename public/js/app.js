c804459dd52fc86b126d4d213da45654eb6ee23e
Tom Yang <xy2286@columbia.edu>
2018-10-08 12:58:03 -0400

    improvement on feature

--- public/js/app.js
+++ public/js/app.js
@@ -1,5 +1,22 @@
 function showPic(link){
-    console.log(link.getAttribute('href'));
+    // console.log(link.getAttribute('href'));
     document.getElementById('image').src = link.getAttribute('href')
+    console.log(link.getAttribute('title'));
+    var desp = document.getElementById('description')
+    desp.firstChild.nodeValue = link.getAttribute('title')
+    // console.log(desp.nodeType);
+    // console.log(desp.nodeName);
+}
 
+window.onload = countBodyChildren
+
+function countBodyChildren(){
+    var body = document.getElementsByTagName('body')[0];
+    console.log(body.childNodes.length);
+    body.childNodes.forEach(
+        function(node){
+            console.log(node.nodeType);
+            console.log(node.nodeName);
+        }
+    )
 }
